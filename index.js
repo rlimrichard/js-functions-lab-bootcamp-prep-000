eec4427abd997a6cc9c7cb96cb5f05ecf8775472
Richard Lim <rlim.richard+flatiron@gmail.com>
2017-09-06 20:35:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function happyHolidaysTo (name){
     return `Happy holidays, ${name}!`
 }
 
-function happyHolidaysTo (holiday, name){
+function happyHolidayTo (holiday, name){
     if (holiday === undefined)
         holiday = "holiday"
 
